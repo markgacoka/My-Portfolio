ec7dfc3da5680609f55c4347488c14df71e9ca82
markgacoka <markgacoka@gmail.com>
2021-03-15 02:26:21 +0300

    Add contact form with POST functionality

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -0,0 +1,36 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    String nameValue = request.getParameter("fname");
+    String replyValue = request.getParameter("reply");
+    System.out.println("Form from: " + nameValue);
+    System.out.println("You submitted: " + replyValue);
+    
+    response.setContentType("text/html;");
+    response.getWriter().println("You submitted: " + nameValue);
+    response.getWriter().println("You submitted: " + replyValue);
+    response.sendRedirect("/");
+  }
+  
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String nameValue = request.getParameter("fname");
+    String replyValue = request.getParameter("reply");
+    System.out.println("Form from: " + nameValue);
+    System.out.println("You submitted: " + replyValue);
+    
+    response.setContentType("text/html;");
+    response.getWriter().println("You submitted: " + nameValue);
+    response.getWriter().println("You submitted: " + replyValue);
+  }
+}
