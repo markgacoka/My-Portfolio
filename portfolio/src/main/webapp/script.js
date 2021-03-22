f273941fe8bd89fc3f1be0b4c5fd0fb426940474
markgacoka <markgacoka@gmail.com>
2021-03-22 21:29:29 +0300

    Add persistent storage functionality

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -19,3 +19,38 @@ function createListElement(text) {
   liElement.innerText = text;
   return liElement;
 }
+
+function loadResponses() {
+  fetch('/list-responses').then(response => response.json()).then((tasks) => {
+    const taskListElement = document.getElementById('task-list');
+    tasks.forEach((task) => {
+      taskListElement.appendChild(createTaskElement(task));
+    })
+  });
+}
+
+function createResponseElement(response) {
+  const taskElement = document.createElement('li');
+  taskElement.className = 'task';
+
+  const titleElement = document.createElement('span');
+  titleElement.innerText = response.title;
+
+  const deleteButtonElement = document.createElement('button');
+  deleteButtonElement.innerText = 'Delete';
+  deleteButtonElement.addEventListener('click', () => {
+    deleteTask(response);
+
+    taskElement.remove();
+  });
+
+  taskElement.appendChild(titleElement);
+  taskElement.appendChild(deleteButtonElement);
+  return taskElement;
+}
+
+function deleteResponse(response) {
+  const params = new URLSearchParams();
+  params.append('id', response.id);
+  fetch('/delete-response', {method: 'POST', body: params});
+}
